36c82ae8a31f21453efa74d1a82f3d9385bb617f
AIDANCHANDLEE <aidan.chandlee@kellogg.northwestern.edu>
2026-03-13 18:44:49 +0000

    Implement places and entries CRUD with user authorization
    - PlacesController: require_login, scope queries to current_user.places
    - EntriesController: require_login, scope place lookup to current_user
    - Place show page uses Bootstrap 3-col grid for entries
    - New Post button and Back to All Places button styled with Bootstrap
    - Entry form uses nested route /places/:place_id/entries
    - Description field uses textarea for long text
    
    >

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,16 +1,25 @@
 class EntriesController < ApplicationController
+  before_action :require_login
 
   def new
+    @place = current_user.places.find_by(id: params["place_id"])
+    if @place.nil?
+      redirect_to "/places"
+    end
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    @place = current_user.places.find_by(id: params["place_id"])
+    if @place.nil?
+      redirect_to "/places"
+    else
+      @entry = @place.entries.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["user_id"] = current_user.id
+      @entry.save
+      redirect_to "/places/#{@place.id}"
+    end
   end
-
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,26 @@
 class PlacesController < ApplicationController
+  before_action :require_login
 
   def index
-    @places = Place.all
+    @places = current_user.places
   end
 
   def show
-    @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    @place = current_user.places.find_by(id: params["id"])
+    if @place.nil?
+      redirect_to "/places"
+    else
+      @entries = @place.entries
+    end
   end
 
   def new
   end
 
   def create
-    @place = Place.new
+    @place = current_user.places.new
     @place["name"] = params["name"]
     @place.save
     redirect_to "/places"
   end
-
 end
